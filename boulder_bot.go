f2e7b5c503e4f01aafc44fee4cf8a136eae22a49
Alwaysgone <ffxiizocker@hotmail.com>
2024-01-20 16:03:46 +0100

    Added sample replies to /help and /status commands

--- boulder_bot.go
+++ boulder_bot.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -14,4 +15,59 @@ func main() {
 	log.Printf("beep boop!")
 
 	bot, err := tg.NewBotAPI(os.Getenv("API_TOKEN"))
+	if err != nil {
+		panic(err)
+	}
+
+	bot.Debug = true
+
+	// Create a new UpdateConfig struct with an offset of 0. Offsets are used
+	// to make sure Telegram knows we've handled previous values and we don't
+	// need them repeated.
+	updateConfig := tg.NewUpdate(0)
+
+	// Tell Telegram we should wait up to 30 seconds on each request for an
+	// update. This way we can get information just as quickly as making many
+	// frequent requests without having to send nearly as many.
+	updateConfig.Timeout = 30
+
+	// Start polling Telegram for updates.
+	updates := bot.GetUpdatesChan(updateConfig)
+
+	// Let's go through each update that we're getting from Telegram.
+	for update := range updates {
+		// Telegram can send many types of updates depending on what your Bot
+		// is up to. We only want to look at messages for now, so we can
+		// discard any other updates.
+		if update.Message == nil {
+			continue
+		}
+
+		if !update.Message.IsCommand() {
+			continue
+		}
+
+		// Create a new MessageConfig. We don't have text yet,
+		// so we leave it empty.
+		msg := tg.NewMessage(update.Message.Chat.ID, "")
+
+		// Extract the command from the Message.
+		switch update.Message.Command() {
+		case "help":
+			msg.Text = "Currently only /status is supported."
+		case "status":
+			msg.Text = "Will create next poll at 2024-01-20 18:00"
+		default:
+			msg.Text = fmt.Sprintf("I don't know how to %s", update.Message.Command())
+		}
+
+		// Okay, we're sending our message off! We don't care about the message
+		// we just sent, so we'll discard it.
+		if _, err := bot.Send(msg); err != nil {
+			// Note that panics are a bad way to handle errors. Telegram can
+			// have service outages or network errors, you should retry sending
+			// messages or more gracefully handle failures.
+			panic(err)
+		}
+	}
 }
