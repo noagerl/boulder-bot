83698a582b6668f9f415520c95c3f12231281263
Alwaysgone <ffxiizocker@hotmail.com>
2024-01-20 15:32:14 +0100

    Added godotenv to read env variables from .env

--- boulder_bot.go
+++ boulder_bot.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
 	log.Printf("beep boop!")
+	log.Printf(os.Getenv("API_TOKEN"))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/noagerl/boulder-bot
 
 go 1.21.6
+
+require github.com/joho/godotenv v1.5.1 // indirect
